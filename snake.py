ef4a7762302c32ea9ff3a85b996458f06254a0f0
DarrenSingh <Singh.Darren0@gmail.com>
2025-11-14 01:30:20 -0800

    docs: add comments to class and functions

--- snake.py
+++ snake.py
@@ -3,17 +3,21 @@ from pygame import Vector2
 
 
 class Snake:
+    """
+    Manages the snake's body, movement and collision logic
+    """
 
-    # methods
     def __init__(self):
-        self.body = deque([Vector2(8, 10), Vector2(7, 10), Vector2(6, 10), Vector2(5, 10), Vector2(4, 10), Vector2(3, 10), Vector2(2, 10)])
+        self.body = deque([Vector2(10, 10), Vector2(9, 10), Vector2(8, 10)])
         self.direction = Vector2(1, 0)
         self.is_growing = False
-        self.speed = 5
+        self.speed = 15
 
     def move(self):
-        # add new block to head in current direction, if not is_growing
-        # call popleft to remove tail
+        """
+        Moves the snake one step in its current direction
+        Handles growing by not popping the tail if is_growing is True
+        """
         head = self.body[0]
         new_head = head + self.direction
         self.body.appendleft(new_head)
@@ -23,18 +27,33 @@ class Snake:
         self.is_growing = False
 
     def grow(self):
+        """
+        Handles setting the state of whether the snake is growing
+        """
         self.is_growing = True
 
-    # updates direction only if new direction is not opposite of the current direction
     def change_direction(self, new_direction: Vector2):
+        """
+        Updates the snakes direction, while preventing 180-degree turns
+        :param new_direction: The new direction to move
+        :type new_direction: Vector2
+        """
         if new_direction != self.direction * -1:
             self.direction = new_direction
 
-    # returns true if the head is in the rest of the body
     def check_collision(self):
-        # check if head Vector2 is in the rest of the body deque
+        """
+        Checks if head Vector2 coordinate exists in the rest of the body deque collection
+        :return: if the snakes head collided with its body
+        :rtype: bool
+        """
         if self.body.count(self.body[0]) > 1:
             return True
 
     def adjust_speed(self, speed_adjustment: int):
+        """
+        Adjust the speed at which the snake moves, relative to the previous speed
+        :param speed_adjustment:
+        :type speed_adjustment: int
+        """
         self.speed += speed_adjustment
